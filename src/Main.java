7b5b3d375631d881b28e0baa8b6d3160c5647ee2
sarraalqahtani <alqahtas@wfu.edu>
2022-02-22 11:09:05 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    static public void main(String args[]){
+        //To collect the execution time;
+        // use the following lines before and after calling the method
+        // you need to compute its execution tim
+        long startTime;
+        long finishTime;
+        startTime = System.nanoTime();
+        //method call
+        finishTime = System.nanoTime();
+
+    }
+}
